5208607d35cc001f2c6c66b518b75946df6cb626
Sulthon <sultona412@gmail.com>
2023-03-07 09:54:39 +0700

    Menambahkan folder dan isi dari folder Bubble

---
+++ Bubble/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+
+array = 0-18-10-10-4-5-20-4-11-15-6-2-17-8-18-16-17-16-20-4;
+
+*/
+
+namespace Sorting
+{
+    class BubbleSort
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 0, 18, 10, 10, 4, 5, 20, 4, 11, 15, 6, 2, 17, 8, 18, 16, 17, 16, 20, 4 };
+            BubbleInt(numbers);
+        }
+
+        static void BubbleInt(int[] arr)
+        {
+            Console.WriteLine("== SEBELUM ==");
+            foreach (int item in arr)
+            {
+                Console.Write($"{item} ");
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < (arr.Length - 1); j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int tampungan = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = tampungan;
+                    }
+                }
+            }
+
+            Console.WriteLine("\n== SESUDAH ==");
+            foreach (int item in arr)
+            {
+                Console.Write($"{item} ");
+            }
+        }
+    }
+}
