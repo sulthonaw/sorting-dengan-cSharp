b0a5b54b87a4ae598311b22d218c4ecc1e974e27
Sulthon <sultona412@gmail.com>
2023-03-07 11:42:39 +0700

    menambahkan method selection untuk int

--- Selection/Program.cs
+++ Selection/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Sorting
+{
+    class Selection
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 0, 18, 10, 10, 4, 5, 20, 4, 11, 15, 6, 2, 17, 8, 18, 16, 17, 16, 20, 4 };
+            SelectionInt(numbers);
+        }
+
+        static void SelectionInt(int[] arr)
+        {
+            Console.WriteLine("== SEBELUM ==");
+            foreach (int item in arr)
+            {
+                Console.Write($"{item} ");
+            }
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                int pos = i;
+                for (int j = i; j < arr.Length; j++)
+                {
+                    if (arr[pos] > arr[j]) pos = j;
+                }
+                if (pos != i)
+                {
+                    int temp = arr[i];
+                    arr[i] = arr[pos];
+                    arr[pos] = temp;
+                }
+            }
+
+            Console.WriteLine("\n== SESUDAH ==");
+            foreach (int item in arr)
+            {
+                Console.Write($"{item} ");
+            }
+        }
+    }
+}
